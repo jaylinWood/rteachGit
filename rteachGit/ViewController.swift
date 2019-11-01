53a4cabebd7af9b87d2aeddaae02146dcfe1888b
getHub <getHub>
2019-11-01 12:45:41 -0400

    Initial Commit

---
+++ rteachGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rteachGit
+//
+//  Created by jaylin wood on 11/1/19.
+//  Copyright © 2019 jaylin wood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
